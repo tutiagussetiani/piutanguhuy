0bce827a320acd87c4c9c1a496930a7609ebd5cd
tutiagusssetiani <tutiagus455@gmail.com>
2022-11-25 11:23:01 +0700

    first commit

---
+++ helpers/Menu.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'failed_jobs', 
+			'label' => 'Failed Jobs', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'migrations', 
+			'label' => 'Migrations', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'password_resets', 
+			'label' => 'Password Resets', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Users', 
+			'icon' => ''
+		)
+	);
+		
+	
+	
+}
